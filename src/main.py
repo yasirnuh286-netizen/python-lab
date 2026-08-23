17d764717bf6a808dd4cb21a01df038e894b70f0
Yasir Mohamed <yasirnuh286@gmail.com>
2026-08-24 01:01:30 +0300

    Add functions in utils.py and test in main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+﻿from utils import square, is_even, celsius_to_fahrenheit
+
+def main():
+    try:
+        num = float(input("Enter a number: "))
+        print(f"Square: {square(num)}")
+        print(f"Is Even: {is_even(num)}")
+        print(f"Fahrenheit: {celsius_to_fahrenheit(num)}")
+    except ValueError:
+        print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,8 @@
+﻿def square(n):
+    return n ** 2
+
+def is_even(n):
+    return n % 2 == 0
+
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
